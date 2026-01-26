0228a68809cf07f8ad1d64ddcaa285528a8779ed
ZXY-0830 <3274592096@qq.com>
2026-01-26 14:14:56 -0600

    commit for adding load file method in files.cs

--- Space Expedition/Files.cs
+++ Space Expedition/Files.cs
@@ -9,7 +9,39 @@ using System.Threading.Tasks;
 namespace Space_Expedition
 {
     internal class Files
-    {   
-        
+    {
+        public static void Load(string path, out ArtifactInventory[] inventory, out int count)
+        {
+            inventory = new ArtifactInventory[20];
+            count = 0;
+            using (StreamReader sr = new StreamReader("galactic_vault.txt"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (count >= inventory.Length)
+                    {
+                        ArtifactInventory[] newInventory = new ArtifactInventory[inventory.Length * 2];
+
+                        for (int i = 0; i < inventory.Length; i++)
+                        {
+                            newInventory[i] = inventory[i];
+                        }
+
+                        inventory = newInventory;
+                    }
+                    string[] array = line.Split('|');
+                    if (array.Length < 5) continue;
+                    string encodedName = array[0].Trim();
+                    string planet = array[1].Trim();
+                    string discoveryDate = array[2].Trim();
+                    string storageLocation = array[3].Trim();
+                    string description = array[4].Trim();
+                    string decodedName = Decoder.DecodeName(encodedName);
+                    ArtifactInventory artifact = new ArtifactInventory(encodedName, planet, discoveryDate, storageLocation, description, decodedName);
+                    inventory[count++] = artifact;
+                }
+            }
+        }
     }
 }
