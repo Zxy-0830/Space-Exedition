99489966e4a5807a328635500f5c5a2c3fd81007
ZXY-0830 <3274592096@qq.com>
2026-01-26 15:11:30 -0600

    commit for add sort and search method

--- Space Expedition/Files.cs
+++ Space Expedition/Files.cs
@@ -52,5 +52,50 @@ namespace Space_Expedition
                 Console.WriteLine($"{artifact.EncodedName} | {artifact.Planet} | {artifact.DiscoveryDate} | {artifact.StorageLocation} | {artifact.Description} ");
             }
         }
+
+        public static void InventorySort(ArtifactInventory[] inventory, int count)
+        {
+            for (int i = 1; i < count; i++)
+            {
+                ArtifactInventory key = inventory[i];
+                string keyName = key.DecodedName;
+
+                int j = i - 1;
+
+                while (j >= 0 && inventory[j].DecodedName.CompareTo(keyName) > 0)
+                {
+                    inventory[j + 1] = inventory[j];
+                    j--;
+                }
+
+                inventory[j + 1] = key;
+            }
+        }
+
+        public static int SearchingByDecodedName(ArtifactInventory[] inventory, int count, string targetDecodedName)
+        {
+            int lo = 0;
+            int hi = count - 1;
+            targetDecodedName = targetDecodedName.ToUpper();
+
+            while (lo <= hi)
+            {
+                int mid = lo + (hi - lo) / 2;
+                int result = inventory[mid].DecodedName.CompareTo(targetDecodedName);
+                if (result == 0)
+                {
+                    return mid;
+                }
+                else if (result > 0)
+                {
+                    hi = mid - 1;
+                }
+                else
+                {
+                    lo = mid + 1;
+                }
+            }
+            return -1;
+        }
     }
 }
