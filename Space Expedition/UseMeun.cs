67f842adc4e1305b5a2bea45d0227e1d67bd93c5
ZXY-0830 <3274592096@qq.com>
2026-01-26 19:42:50 -0600

    commit for meun.cs

--- Space Expedition/UseMeun.cs
+++ Space Expedition/UseMeun.cs
@@ -8,6 +8,34 @@ namespace Space_Expedition
 {
     internal class UseMeun
     {
-       
+        public static void Run(ref ArtifactInventory[] inventory, ref int count)
+        {
+            while (true)
+            {
+                Console.WriteLine("\nWelcome to use the Space Expedition Manage System! ");
+                Console.WriteLine("1.Add new artifact. ");
+                Console.WriteLine("2.View inventory. ");
+                Console.WriteLine("0.Exit ");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Files.Adding(ref inventory, ref count);
+                        Console.WriteLine("Added.");
+                        break;
+                    case "2":
+                        Files.Displaying(inventory, count);
+                        break;
+                    case "0":
+                        Console.WriteLine("Exit. ");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid enter. ");
+                        break;
+                }
+            }
+        }
     }
 }
